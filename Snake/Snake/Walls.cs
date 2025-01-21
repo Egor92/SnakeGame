ee58c4687b139b7cad93da8c1724bf6e0d24ea83
Polina Levkovich <levkovich.polina@gmail.com>
2025-01-21 17:06:30 +0300

    Snake game

---
+++ Snake/Snake/Walls.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game
+{
+    internal class Walls
+    {
+        public void DrawWalls(int width, int height)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                new Pixel(i, 0).Draw('#');
+                new Pixel(i, height - 1).Draw('#');
+            }
+            for (int i = 0; i < height; i++)
+            {
+                new Pixel(0, i).Draw('#');
+                new Pixel(width - 1, i).Draw('#');
+            }
+        }
+    }
+}
