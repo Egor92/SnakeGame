9eddfb8bf07c7fe44713d60352e74e6995a523ee
Polina Levkovich <levkovich.polina@gmail.com>
2025-01-22 23:40:47 +0300

    Snake game

---
+++ Snake/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Console;
+namespace Snake_Game
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CursorVisible = false;
+            int width = 45;
+            int height = 15;
+
+            Walls walls = new Walls();
+            walls.DrawWalls(width, height);
+            Snake snake = new Snake(width / 2, height / 2);
+            snake.Draw();
+
+            Foods food = new Foods();
+            int foodCount = 5;
+            food.Spawn(width, height, snake.GetBody(), foodCount);
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKey key = Console.ReadKey(true).Key;
+                    snake.ChangeDirection(key);
+                }
+
+                snake.Direction();
+
+                if (snake.CheckWalls(width, height))
+                {
+                    string message = "Вы врезались в стенку. Игра закончена!";
+                    int x = (width - message.Length) / 2;
+                    int y = height / 2;
+                    SetCursorPosition(x, y);
+                    WriteLine(message);
+                    break;
+                }
+
+                Pixel eatenFood = null;
+                foreach (var foodItem in food.FoodItems)
+                {
+                    if (snake.CheckCollisionWithFood(foodItem))
+                    {
+                        eatenFood = foodItem;
+                        break;
+                    }
+                }
+                if (eatenFood != null)
+                {
+                    snake.Grow();
+                    food.Remove(eatenFood);
+
+                    if (food.FoodItems.Count < foodCount)
+                    {
+                        food.Spawn(width, height, snake.GetBody(), foodCount - food.FoodItems.Count);
+                    }
+                }
+
+                Thread.Sleep(200);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Snake/Snake.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game
+{
+    internal class Snake
+    {
+        private List<Pixel> snake;
+        private Direction CurrentDirection;
+
+        public Snake(int startX, int startY)
+        {
+            snake = new List<Pixel>
+            {
+                new Pixel(startX, startY),
+                new Pixel(startX - 1, startY),
+                new Pixel(startX - 2, startY)
+            };
+
+            CurrentDirection = Snake_Game.Direction.Up;
+        }
+
+        public void Draw()
+        {
+            foreach (var pixel in snake)
+            {
+                pixel.Draw('*');
+            }
+        }
+
+        public void Direction()
+        {
+            var head = snake[0];
+            int newX = head.X;
+            int newY = head.Y;
+
+            switch (CurrentDirection)
+            {
+                case Snake_Game.Direction.Up:
+                    newY--;
+                    break;
+                case Snake_Game.Direction.Down:
+                    newY++;
+                    break;
+                case Snake_Game.Direction.Left:
+                    newX--;
+                    break;
+                case Snake_Game.Direction.Right:
+                    newX++;
+                    break;
+            }
+
+            var newHead = new Pixel(newX, newY);
+            snake.Insert(0, newHead);
+            snake[snake.Count - 1].Clear();
+            snake.RemoveAt(snake.Count - 1);
+
+            Draw();
+        }
+
+        public void ChangeDirection(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.UpArrow when CurrentDirection != Snake_Game.Direction.Down:
+                    CurrentDirection = Snake_Game.Direction.Up;
+                    break;
+                case ConsoleKey.DownArrow when CurrentDirection != Snake_Game.Direction.Up:
+                    CurrentDirection = Snake_Game.Direction.Down;
+                    break;
+                case ConsoleKey.LeftArrow when CurrentDirection != Snake_Game.Direction.Right:
+                    CurrentDirection = Snake_Game.Direction.Left;
+                    break;
+                case ConsoleKey.RightArrow when CurrentDirection != Snake_Game.Direction.Left:
+                    CurrentDirection = Snake_Game.Direction.Right;
+                    break;
+            }
+        }
+
+        public bool CheckWalls(int width, int height)
+        {
+            var head = snake[0];
+
+            if (head.X <= 0 || head.X >= width - 1 || head.Y <= 0 || head.Y >= height - 1)
+            {
+                return true;
+
+            }
+
+            for (int i = 1; i < snake.Count; i++)
+            {
+                if (head.X == snake[i].X && head.Y == snake[i].Y)
+                    return true;
+            }
+
+            return false;
+        }
+        public void Grow()
+        {
+            var tail = snake[snake.Count - 1];
+            snake.Add(new Pixel(tail.X, tail.Y));
+        }
+        public bool CheckCollisionWithFood(Pixel foodPosition)
+        {
+            var head = snake[0];
+            return head.X == foodPosition.X && head.Y == foodPosition.Y;
+        }
+
+        public List<Pixel> GetBody()
+        {
+            return snake;
+        }
+
+
+    }
+}
