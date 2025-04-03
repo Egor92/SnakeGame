579d9d7c4c35e21bed67e8b59dd576ff129a1ecb
Polina Levkovich <levkovich.polina@gmail.com>
2025-04-03 15:33:20 +0300

    Изменила название

--- src/multiplatform/Snake.Multiplatform/Snake.ConsoleApp/Game.cs
+++ src/multiplatform/Snake.Multiplatform/Snake.ConsoleApp/Game.cs
@@ -1,7 +1,6 @@
-﻿using Snake.ConsoleApp;
-using Snake.Logic;
+﻿using Snake.Logic;
 
-namespace Snake;
+namespace Snake.ConsoleApp;
 
 public class Game(GameData gameData, GameLogic gameLogic, int timeBetweenSteps)
 {
