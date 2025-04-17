53d96cfb30a1ced022418c56333708bfaca6ea64
Polina Levkovich <levkovich.polina@gmail.com>
2025-04-17 14:35:20 +0300

    Создала enum #85

---
+++ src/multiplatform/Snake.ConsoleApp/CellObject.cs
@@ -0,0 +1,22 @@
+﻿namespace Snake.ConsoleApp;
+
+public enum CellObject
+{
+    Empty,
+    Wall,
+    Food,
+    SnakeHeadLooksUp,
+    SnakeHeadLooksDown,
+    SnakeHeadLooksLeft,
+    SnakeHeadLooksRight,
+    SnakeTailLooksUp,
+    SnakeTailLooksDown,
+    SnakeTailLooksLeft,
+    SnakeTailLooksRight,
+    SnakeBodyHorizontal,
+    SnakeBodyVertical,
+    SnakeTurnDownOrLeft,
+    SnakeTurnDownOrRight,
+    SnakeTurnUpOrLeft,
+    SnakeTurnUpOrRight
+}
