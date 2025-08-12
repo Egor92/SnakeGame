f5b132e387508305890e59f0533cd74e5df9808f
Polina Levkovich <levkovich.polina@gmail.com>
2025-08-12 15:28:54 +0300

    Сделала реагирование на кнопки смены направления, змейка может менять направление #77

--- src/multiplatform/Snake.DesktopApp/MainWindow.xaml.cs
+++ src/multiplatform/Snake.DesktopApp/MainWindow.xaml.cs
@@ -1,11 +1,38 @@
-﻿namespace Snake.DesktopApp;
+﻿using System.Windows.Input;
+using Snake.DesktopApp.ViewModels;
+using Snake.Logic;
+
+namespace Snake.DesktopApp;
 
 public partial class MainWindow
 {
-    public MainWindow()
+    private readonly GameViewModel _viewModel;
+    private readonly GameLogic _gameLogic;
+
+    public MainWindow(GameViewModel viewModel, GameLogic gameLogic)
     {
+        _viewModel = viewModel;
+        _gameLogic = gameLogic;
         InitializeComponent();
         Width = 434;
         Height = 480;
+        PreviewKeyDown += OnPreviewKeyDown;
+    }
+
+    private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        Direction? requestedDirection = e.Key switch
+        {
+            Key.Up => Direction.Up,
+            Key.Down => Direction.Down,
+            Key.Left => Direction.Left,
+            Key.Right => Direction.Right,
+            _ => null
+        };
+
+        if (requestedDirection != null)
+        {
+            _gameLogic.ChangeDirection(requestedDirection.Value);
+        }
     }
 }
